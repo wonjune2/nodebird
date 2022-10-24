f03fc29bfc049dfcc9db2b45216922cf2dd057f9
wonjoon <zzzdnjswns@naver.com>
2022-10-24 20:37:11 +0900

    database init

---
+++ models/hashtag.js
@@ -0,0 +1,26 @@
+const Sequelize = require("sequelize");
+
+module.exports = class Hashtag extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        title: {
+          type: Sequelize.STRING(15),
+          alloNull: false,
+          unique: true,
+        },
+      },
+      {
+        sequelize,
+        timestamps: true,
+        underscored: false,
+        modelName: "Hashtag",
+        tableName: "hashtags",
+        paranoid: false,
+        charset: "utf8mb4",
+        collate: "utf8mb_general_ci",
+      }
+    );
+  }
+  static associate(db) {}
+};
