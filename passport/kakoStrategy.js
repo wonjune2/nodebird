49bd5287f49e6559d4f9bea09f15fe098b1ecf94
wonjoon <zzzdnjswns@naver.com>
2022-10-30 00:04:12 +0900

    kakaoStrategy

--- passport/kakoStrategy.js
+++ passport/kakoStrategy.js
@@ -2,3 +2,36 @@ const passport = require("passport");
 const kakaoStrategy = require("passport-kakao");
 
 const User = require("../modles/user");
+
+module.exports = () => {
+  passport.use(
+    new kakaoStrategy(
+      {
+        clientID: process.env.KAKAO_ID,
+        callbackURL: "/auth/kakao/callaback",
+      },
+      async (acessToken, refreshToken, profile, done) => {
+        console.log("kako profile", profile);
+        try {
+          const exUser = await User.findOne({
+            where: { snsId: profile.id, provider: "kakao" },
+          });
+          if (exUser) {
+            done(null, exUser);
+          } else {
+            const newUser = await User.create({
+              email: profile._json && profile._json.kakao_account_email,
+              nick: profile.displayName,
+              snsId: profile.id,
+              provider: "kakao",
+            });
+            done(null, newUser);
+          }
+        } catch (error) {
+          console.error(error);
+          done(error);
+        }
+      }
+    )
+  );
+};
