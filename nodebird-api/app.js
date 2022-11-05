938d7e395091904d5a8c7251bf90ac540acee676
joon <zzzdnjswns@naver.com>
2022-11-05 11:24:55 +0900

    api

--- nodebird-api/app.js
+++ nodebird-api/app.js
@@ -1,55 +1,58 @@
 const express = require("express");
-const cookieParser = require("cookie-parser");
-const morgan = require("morgan");
 const path = require("path");
+const cookieParser = require("cookie-parser");
+const passport = require("passport");
+const morgan = require("morgan");
 const session = require("express-session");
 const nunjucks = require("nunjucks");
 const dotenv = require("dotenv");
-const passport = require("passport");
-
-const { sequelize } = require("./models");
-const passportConfig = require("./passport");
-const authRouter = require("./routes/auth");
-const pageRouter = require("./routes/page");
-const postRouter = require("./routes/post");
-const userRouter = require("./routes/user");
 
 dotenv.config();
+const authRouter = require("./routes/auth");
+const indexRouter = require("./routes");
+const { sequelize } = require("./models");
+const passportConfig = require("./passport");
+const { addHook } = require("./models/user");
 
 const app = express();
 passportConfig();
-app.set("port", process.env.PORT || 8001);
-app.set("view engine", "html");
-nunjucks.configure("views", { express: app, watch: true });
+app.set("port", process.env.PORT || 8002);
+app.set("view engine", html);
+nunjucks.configure("views", {
+  express: app,
+  watch: true,
+});
 sequelize
-  .sync({ force: true })
+  .sync({ force: false })
   .then(() => {
     console.log("데이터베이스 연결 성공");
   })
   .catch((err) => {
     console.error(err);
   });
+
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/img", express.static(path.join(__dirname, "uploads")));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+add.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(
   session({
     resave: false,
     saveUninitialized: false,
     secret: process.env.COOKIE_SECRET,
-    cookie: { httpOnly: true, secure: false },
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
   })
 );
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use("/", pageRouter);
 app.use("/auth", authRouter);
-app.use("/post", postRouter);
-app.use("/user", userRouter);
+app.use("/", indexRouter);
+
 app.use((req, res, next) => {
   const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
   error.status = 404;
@@ -58,7 +61,7 @@ app.use((req, res, next) => {
 
 app.use((err, req, res, next) => {
   res.locals.message = err.message;
-  res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
+  res.locals.error = process.env.NODE_ENV !== "product" ? err : {};
   res.status(err.status || 500);
   res.render("error");
 });
